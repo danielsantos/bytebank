a5d9604a3a2e28db11129dabc688b04bee15d257
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-06 15:17:33 -0300

    usando MaterialApp com widget Text de home com o MaterialApp o Text não precisa ter definido a direção

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        )
-      ],
-    ));
+void main() => runApp(MaterialApp(
+  home: Text('Test')
+));
