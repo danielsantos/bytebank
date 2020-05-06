0e745d1d78a149738e41e50a93e32ba246caea99
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-06 14:42:48 -0300

    widget basic's Column - by flutter.dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr,));
-
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: const FlutterLogo(),
+          ),
+        ),
+      ],
+    ));
