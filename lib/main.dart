d6f53666f202050e6d0d3792395c342e33940723
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-06 15:48:03 -0300

    adicionando um Text ao body do widget Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() =>
+    runApp(MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transferências'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    )));
+          body: Text('test'),
+          appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+        )));
