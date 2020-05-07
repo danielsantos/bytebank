fa9f435f2ad77b575be84892674f7429a1b79568
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-06 22:17:12 -0300

    Refactoring.. create a new Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
         home: Scaffold(
-      body: ListaTransferencias(),
+          body: FormularioTransferencia(),
+        ));
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1234)),
+          ItemTransferencia(Transferencia(200.0, 9876)),
+          ItemTransferencia(Transferencia(450.0, 4308))
+        ],
+      ),
       appBar: AppBar(
         title: Text('Transferências'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-    )));
-
-class ListaTransferencias extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 1234)),
-        ItemTransferencia(Transferencia(200.0, 9876)),
-        ItemTransferencia(Transferencia(450.0, 4308))
-      ],
     );
   }
 }
