c39e481e07cb7bfa7949f0345b26aa297898a7eb
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-14 17:14:07 -0300

    chamando o formulário a partir da lista de transferências, e passando para a lista os dados preenchidos no formulário

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: FormularioTransferencia(),
+      body: ListaTransferencias(),
     ));
   }
 }
@@ -38,27 +38,30 @@ class FormularioTransferencia extends StatelessWidget {
             ),
             RaisedButton(
               child: Text('Confimar'),
-              onPressed: () => _criaTransferencia(),
+              onPressed: () {
+                _criaTransferencia(context);
+              },
             ),
           ],
         ));
   }
 
-  void _criaTransferencia() {
+  void _criaTransferencia(BuildContext context) {
     print('clicou no confirmar');
     debugPrint(_controladorCampoNumeroConta.text);
 
     final int numeroConta =
-        int.tryParse(_controladorCampoNumeroConta.text);
+    int.tryParse(_controladorCampoNumeroConta.text);
     final double valor =
-        double.tryParse(_controladorCampoValor.text);
+    double.tryParse(_controladorCampoValor.text);
 
     if (numeroConta != null && valor != numeroConta) {
+      debugPrint('criando a transferência');
       final transferenciaCriada = Transferencia(valor, numeroConta);
       debugPrint('$transferenciaCriada');
+      Navigator.pop(context, transferenciaCriada);
     }
   }
-
 }
 
 class Editor extends StatelessWidget {
@@ -103,6 +106,16 @@ class ListaTransferencias extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () {
+          final Future<Transferencia> future = Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return FormularioTransferencia();
+            })
+          );
+          future.then((transferenciaRecebida) {
+            debugPrint('chegou no future');
+            debugPrint('$transferenciaRecebida');
+          });
+        },
       ),
     );
   }
